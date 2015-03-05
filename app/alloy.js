5936d48fa43589bac9a2959458e2accc94af11d5
Gregori Faroux <gfaroux@Gregoris-iMac.dyn.gsu.edu>
2015-03-05 12:04:10 -0500

    first commit

---
+++ app/alloy.js
@@ -0,0 +1,4 @@
+
+Alloy.Globals.likedColor = "#0C0";
+Alloy.Globals.defaultColor = "#CCC";
+
---
+++ app/controllers/index.js
@@ -0,0 +1,13 @@
+function like(e) {
+	setTimeout(function() {
+		Ti.API.info("like");
+		e.source.backgroundColor = (e.source.backgroundColor === Alloy.Globals.defaultColor) ? Alloy.Globals.likedColor : Alloy.Globals.defaultColor;
+	},5000);
+	
+}
+
+function optimizedLike(e) {
+	e.source.backgroundColor = (e.source.backgroundColor === Alloy.Globals.defaultColor) ? Alloy.Globals.likedColor : Alloy.Globals.defaultColor;
+	Ti.API.info("optimized like");
+}
+$.index.open();
